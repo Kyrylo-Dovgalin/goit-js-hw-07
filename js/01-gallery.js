c973f1cebfe854f5cc2a9b72c1abd0b6a83e0acb
Kyrylo Dovgalin <kyrylo.dovgalin@gmail.com>
2022-10-24 15:55:32 +0300

    Tasks # 1, 2 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+refs.gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+refs.gallery.addEventListener("click", onGalleryClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({
+        preview,
+        original,
+        description,
+      }) => `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`
+    )
+    .join(" ");
+}
+
+function onGalleryClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const source = evt.target.dataset.source;
+
+  openModal(source);
+}
+
+function openModal(source) {
+  basicLightbox
+    .create(
+      `
+		<img width="1400" height="900" src="${source}">
+	`
+    )
+    .show();
+  //   console.log(basicLightbox.visible());
+}
+
+// console.log(basicLightbox.visible());
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+refs.gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+refs.gallery.addEventListener("click", onGalleryClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({
+        preview,
+        original,
+        description,
+      }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+    .join(" ");
+}
+
+function onGalleryClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return console.log(`not that`);
+  }
+  const source = evt.target.dataset.source;
+}
+
+// var lightbox = $(".gallery a").simpleLightbox({
+//   /* options */
+// });
+
+// lightbox();
+
+let gallery = new SimpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", function () {
+  // do something…
+});
+
+gallery.on("error.simplelightbox", function (e) {
+  console.log(e); // some usefull information
+});
